2f317680b8e1bbf54f1ca75efcacba4779f40fe7
Marthenn <bintang.dwimarthen@gmail.com>
2022-08-14 22:42:33 +0700

    merapikan directory

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,61 @@
+const {
+    addBookHandler,
+    getAllBooksHandler,
+    showBookHandler,
+    updateBookHandler,
+    deleteBookHandler,
+} = require('./handler');
+
 const routes = [
     {
         method: 'POST',
         path: '/books',
-        handler: () => {},
+        handler: addBookHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: () => getAllBooksHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: () => showBookHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: () => updateBookHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: () => deleteBookHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
     },
 ];
 
